afa38783dfe8af3139e6e7f4e1b9defa420e31c4
Stefan356 <stefan.schwaiger@students.fh-hagenberg.at>
2022-11-30 11:49:32 +0100

    setup done, images only generated once
    removed unnecessary files

--- Parchment-Dateien/ie.js
+++
@@ -1,2 +0,0 @@
-function r(){$("#errorcontent").text("Sorry, but Parchment depends on web features unsupported by this browser. Please try a more modern browser."),$("#errorpane").show()}$(r);
-//# sourceMappingURL=ie.js.map
--- main.py
+++ main.py
@@ -1,20 +1,117 @@
-import cv2
-import pytesseract
-import pyscreenshot as ImageGrab
+#Importieren der Module
+import PIL.ImageShow
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+import keyboard
+import codecs
+import time
+import json
+import requests
+import io
+import base64
+from PIL import Image, PngImagePlugin
 
-#grab part of the screen
-im = ImageGrab.grab(bbox=(300,300,1600,1600))
-im.show()
+from bs4 import BeautifulSoup
 
-#store in a file
-im.save('screen.png')
 
-#what image should be read
-img = cv2.imread('screen.png')
+# Optionen die für den Web Treiber gesetzt werden können...
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ['enable-automation'])
+driver = webdriver.Chrome(options = options)
 
-#image to string
-text = pytesseract.image_to_string(img)
 
-print(text)
+#Url aufrufen
+#driver.get("http://localhost/iplayif/parchment.html")
+driver.get("https://iplayif.com/")
+
+#Link wird in Eingabefeld kopiert und Enter gedrückt = Seite lädt Spiel
+elem = driver.find_element(By.ID, "play-url-input")
+elem.send_keys("https://eblong.com/zarf/ftp/dreamhold.z8")
+elem.send_keys(Keys.RETURN)
+
+
+#Variable für While Schleife
+gameRunning = True
+
+def GenerateImage(prompt, title):
+    # run locally
+    url = "http://127.0.0.1:7860"
+
+    # run via FHOOE-Vpn
+    #url = "http://10.21.3.217:7860"
+
+    payload = {
+        "prompt": prompt,
+        "steps": 25
+    }
+
+    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+
+    r = response.json()
+    load_r = json.loads(r['info'])
+    meta = load_r["infotexts"][0]
+
+    filename = f"{title}.png"
+
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", meta)
+        image.save(filename, pnginfo=pnginfo)
+
+# list storing titles of already generated images
+already_generated = []
+
+# show image of starting area for Dreamhold
+desc = 'This space, barely wider than outstretched arms, seems to have been chopped raw and square from unfinished stone. Only the floor is smooth -- a fine white surface beneath your feet. There is a narrow gap in the east wall.'
+name = 'cell'
+GenerateImage(desc, name)
+already_generated.append(name)
+image = Image.open('cell.png')
+image.show()
+
+# game loop
+while gameRunning:
+    time.sleep(0.25)
+
+    #Warten auf Keyboard input
+    while True:
+        if keyboard.is_pressed("return"):
+
+            # open html - aktuell laufendes HTML
+            doc = driver.page_source
+
+            # Initialize the object with the document
+            soup = BeautifulSoup(doc, "html.parser")
+
+            # find last subheader in html (i.e. the name of the current room the player is in)
+            last_subheader = soup("div", {"class": "Style_subheader"})[-1]
+
+            # get text of next 'div'-element (description of current room)
+            desc = last_subheader.find_next('div').text
+            name = last_subheader.text
+
+            open('file.txt', 'w').close()
+            with open("file.txt", "w") as f:   #("D:\\Daten\\FH dump\\Nextcloud Folder\\Pro5\\Pro5_test_project\\file.txt", "w")
+                f.write(desc)
+            print(name)
+            print(desc)
+
+            #generate and show image
+            if name not in already_generated:
+                GenerateImage(desc, name)
+                already_generated.append(name)
+
+            image = Image.open(f'{name}.png')
+            image.show()
+            break
+
+        if keyboard.is_pressed("esc"):
+            gameRunning = False
+            print("Game Ended")
+            break
+
+
+   
